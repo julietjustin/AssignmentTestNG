4d4fb91d53ebc3d359c3c25ed25e1ebaf2036702
julietjustin <julietkurian999@gmail.com>
2022-11-01 16:24:30 +0530

    Navigation edited

--- Navigation.java
+++ Navigation.java
@@ -6,9 +6,9 @@ import org.testng.annotations.Test;
 public class Navigation extends Base {
 	@Test
 	public void firstPageTitle() {
-		String actualTitle,ExpTitle="Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
+		String actualTitle,expectedTitle="Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
 		actualTitle=driver.getTitle();
-		Assert.assertEquals(actualTitle, ExpTitle);
+		Assert.assertEquals(actualTitle, expectedTitle);
 		}
 	@Test
 	public void firstPageURL() {
@@ -30,15 +30,20 @@ public class Navigation extends Base {
 	}
 	@Test
 	public void getPageSource() {
-		String pageSource;
-		pageSource=driver.getPageSource();
-		Assert.assertTrue(true);
+		String actualPageSource,expectedPageSource="stylesheet";
+		//getting page source 
+		actualPageSource=driver.getPageSource();
+		Assert.assertTrue(actualPageSource.contains(expectedPageSource), "Page Source Is Not Found");
 	}
+	
 	@Test
 	public void navigateToLoginPage() {
+		String actualURL,expectedURL="https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&";
 		driver.navigate().to("https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&");
-		Assert.assertTrue(true);
+		actualURL=driver.getCurrentUrl();
+		Assert.assertEquals(actualURL, expectedURL, "expected url is not correct");
 	}
+	
 
 
 }
